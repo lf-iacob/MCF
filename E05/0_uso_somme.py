4fd49393f6bdd8b72d1107582ae77b1057e961b0
lf-iacob <iacob.laura.f@gmail.com>
2025-01-18 16:19:27 +0100

    Update 0_uso_somme.py

--- E05/0_uso_somme.py
+++ E05/0_uso_somme.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import somme as s
 
 """##Esercizio 0 - Funzioni e Moduli
 
@@ -12,42 +13,6 @@ Creare uno script python che importi il modulo somme appena creato e ne utilizzi
 
 Esaminare la cartella di lavoro
 """
-
-#MODULO: somme.py
-
-#Funzione 1
-def somman(x):
-  sum=0
-  for i in range (0,x+1):
-    sum-sum+i
-  return sum
-
-#Funzione 2
-def sommasqrt(x):
-  sum=0
-  for i in range (0, x+1):
-    sum=sum+np.sqrt(i)
-  return sum
-
-#Funzione 3
-def sommaprodotto(x):
-  s=0
-  p=1
-  for i in range(1, x+1):
-    s=s+i
-    p=p*i
-  return s, p
-
-#Funzione 4
-def geometrica(x, alpha=1):
-  sum=0
-  for i in range (1, x+1):
-    sum=sum+pow(i, alpha)
-  return sum
-
-#ESERCIZIO 0_uso_somme.py
-
-import somme as s
 
 print('Somma dei primi n naturali')
 n=input('-> Inserisca il numero naturale: ')
